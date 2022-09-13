754ec556fe2f6a537e577089c9fafe021aa4eb63
Elton Bautista <eltonbestaha@gmail.com>
2022-09-13 06:23:13 -0600

    Added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+function Navbar() {
+  return (
+    <header className="grid justify-items-center">
+      <nav className="navbar max-w-screen-xl">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
