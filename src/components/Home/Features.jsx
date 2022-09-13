a608f70b03b501a882166edd161f2e69d1fc887d
Elton Bautista <eltonbestaha@gmail.com>
2022-09-13 13:40:37 -0600

    DRY

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -4,14 +4,14 @@ import FeatureChild from "./FeatureChild";
 
 export default function Features() {
   return (
-    <section className="mx-auto max-w-screen-xl py-12 text-blue-700 sm:py-16 lg:py-20">
+    <section className="mx-auto max-w-screen-xl py-12 sm:py-16 lg:py-20 text-base-content">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
-          <p className="text-sm font-medium text-blue-800">INTRODUCING</p>
+          <p className="text-sm font-medium ">INTRODUCING</p>
           <h2 className="mt-2 text-3xl font-bold sm:text-4xl xl:text-5xl">
             The Future of Analytics
           </h2>
-          <hr className="mx-auto mt-4 h-2 w-32 border-none bg-blue-700" />
+          <hr className="mx-auto mt-4 h-2 w-32 border-none " />
         </div>
 
         <div className="mt-10 grid grid-cols-1 gap-20 text-center sm:mx-auto sm:max-w-sm md:mt-20 md:max-w-full md:text-left">
@@ -27,42 +27,23 @@ export default function Features() {
               src="/favicon.svg"
               alt=""
             />
-            <div className="sm:ml-10">
-              <h3 className="text-4xl">Get advantage over others</h3>
-              <p className="mt-6 text-base text-gray-500">
+            <div className="">
+              <h3 className="text-4xl ">Get advantage over others</h3>
+              <p className="mt-6 text-base text-accent">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
                 ipsa delectus quod?
               </p>
-              <Button
-                color="primary"
-                hoverColor="accent"
-                link="/"
-                title="Learn more"
-                direction="left"
-              />
+              <Button link="/" title="Learn more" />
             </div>
           </div>
 
           <div className="flex flex-col items-center justify-center md:flex-row">
-            <img
-              className="md:ml-10 h-56 object-contain md:order-2 md:object-left"
-              src="/favicon.svg"
-              alt=""
+            <FeatureChild
+              headerText="Foo"
+              imgAlt=""
+              imgSrc="/favicon.svg"
+              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ipsa delectus quod?"
             />
-            <div className="">
-              <h3 className="text-4xl">Recover lost revenue</h3>
-              <p className="mt-6 text-base text-gray-500">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                ipsa delectus quod?
-              </p>
-              <Button
-                color="primary"
-                hoverColor="accent"
-                link="/"
-                title="Learn more"
-                direction="left"
-              />
-            </div>
           </div>
         </div>
       </div>
