001e7ddfcacf7df4c230e4e5a43eb1d885875ba8
Elton Bautista <eltonbestaha@gmail.com>
2022-09-13 11:10:08 -0600

    Created Button

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default function Button({ title, link }) {
+  return (
+    <button className="mt-3">
+      <a
+        class="relative inline-block px-8 py-3 overflow-hidden border border-primary group focus:outline-none focus:ring"
+        href={link}
+      >
+        <span class="absolute inset-y-0 left-0 w-[2px] transition-all bg-primary group-hover:w-full group-active:bg-primary-content"></span>
+
+        <span class="relative text-sm font-medium text-primary transition-colors group-hover:text-base-100">
+          {title}
+        </span>
+      </a>
+    </button>
+  );
+}
