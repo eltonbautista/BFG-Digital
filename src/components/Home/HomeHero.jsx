e2bc854657b97b2bfbaf63e81c981c06ce57bbcc
Elton Bautista <eltonbestaha@gmail.com>
2022-09-20 07:33:46 -0600

    Changed font-size and pricing

--- src/components/Home/HomeHero.jsx
+++ src/components/Home/HomeHero.jsx
@@ -93,7 +93,7 @@ export default function Hero() {
                 <a
                   href="/calgary-web-design-company/web-development/#serviceSteps"
                   aria-label=""
-                  className="group inline-flex items-center uppercase px-4 pl-0 self-center md:pl-4 mt-4 md:mt-1 hover:underline font-medium"
+                  className="group inline-flex items-center uppercase px-4 pl-0 self-center md:pl-4 mt-4 md:mt-1 hover:underline font-medium text-sm lg:text-base"
                 >
                   How our websites generate business for you
                   <svg
