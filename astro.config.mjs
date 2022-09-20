77b4144790d06004cb727936a438144591e1cf01
Elton Bautista <eltonbestaha@gmail.com>
2022-09-20 07:42:11 -0600

    Added SEO dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import robotsTxt from "astro-robots-txt";
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: "https://bfginteractive.ca/",
+  experimental: {
+    integrations: true,
+  },
+  integrations: [
+    tailwind(),
+    react(),
+    robotsTxt({
+      sitemap: false,
+    }),
+    sitemap(),
+  ],
 });
